f83e7f00d7f03a3fe60ba1a208b777205507d943
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-11-02 21:04:54 -0600

    Agregando ejemplo de ref

--- sesion04/03-refs/src/components/app.js
+++ sesion04/03-refs/src/components/app.js
@@ -1,40 +1,73 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function App() {
+	console.log("Renderizando...");
+
 	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
+
+	const usernameRef = useRef();
+	const passwordRef = useRef();
 
 	function handleSubmit(event) {
 		event.preventDefault();
+
+		const usernameInput = usernameRef.current;
+		const passwordInput = passwordRef.current;
+
+		const username = usernameInput.value;
+		const password = passwordInput.value;
+
+		if (!username) {
+			//.focus() es del DOM
+			usernameInput.focus();
+		} else if (!password || password.length < 8) {
+			passwordInput.focus();
+		} else {
+			usernameInput.value = "";
+			passwordInput.value = "";
+		}
+	}
+
+	function handleUsername(event) {
+		const text = event.target.value;
+
+		if (text.includes(".")) {
+			const newText = text.replace(".", "");
+			setUsername(newText);
+		} else {
+			setUsername(text);
+		}
 	}
 
 	return (
-		<div class="d-flex justify-content-center align-items-center vw-100 vh-100">
-			<form onSubmit={handleSubmit}>
+		<div className="d-flex justify-content-center align-items-center vw-100 vh-100">
+			<form onSubmit={handleSubmit} noValidate>
 				<div className="mb-3">
 					<label htmlFor="exampleInputEmail1" className="form-label">
-						Email address
+						Username
 					</label>
 					<input
-						type="email"
+						type="text"
 						className="form-control"
 						id="exampleInputEmail1"
 						aria-describedby="emailHelp"
-						onChange={(e) => setUsername(e.target.value)}
+						ref={usernameRef}
+						value={username}
+						onChange={handleUsername}
 					/>
 					<div id="emailHelp" className="form-text">
 						We'll never share your email with anyone else.
 					</div>
 				</div>
 				<div className="mb-3">
-					<label for="exampleInputPassword1" className="form-label">
+					<label htmlFor="exampleInputPassword1" className="form-label">
 						Password
 					</label>
 					<input
 						type="password"
 						className="form-control"
 						id="exampleInputPassword1"
-						onChange={(e) => setPassword(e.target.value)}
+						ref={passwordRef}
 					/>
 				</div>
 				<button type="submit" className="btn btn-primary">
